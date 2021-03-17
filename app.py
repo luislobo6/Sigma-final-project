027aa1380a8f1b79b563777d1c6e0de4a3b67e0a
unknown <el.lobo.i@gmail.com>
2021-03-16 20:59:36 -0600

    with more methods to query

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import system
 from flask import Flask, render_template
 import pandas as pd
 from sqlalchemy import create_engine
@@ -74,6 +75,37 @@ def information(bank_concept):
     FROM data_filtered_bank
     WHERE cve_institucion = CAST ({bank} AS TEXT )
     AND cve_concepto = {concept};""",engine)
+
+    # close connection
+    # engine.drop
+
+    # write it as a json object and return it
+    return json.dumps(test.to_json())
+
+# In this URL you can get specific information regarding one bank_concept, example to try: /more/21_102_40002-100000000000_110000000000
+@app.route("/more/<bank_concept>")
+def information_more(bank_concept):
+    
+    params = bank_concept.split("-")
+    banks = params[0].split("_")
+    concepts = params[1].split("_")
+
+    # database connection
+    engine = create_engine(CONN)
+
+    # filter query with the bank_id and id_concept received in the URL
+    query1 = """SELECT * 
+    FROM data_filtered_bank
+    WHERE cve_institucion IN ({}) """.format(', '.join(['%s' for _ in banks]))
+
+    query2 = "AND cve_concepto IN ({});".format(', '.join(['%s' for _ in concepts]))
+
+    query_final = f"{query1}{query2}"
+    params_final = banks + concepts
+
+    print("*************************",query_final, params_final,"****************************")
+
+    test = pd.read_sql(query_final,engine, params=[params_final])
 
     # close connection
     # engine.drop
